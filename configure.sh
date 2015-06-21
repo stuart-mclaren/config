6a66de46037754983c948b4dff69297c28097447
sam <sam@hijack.(none)>
2015-06-21 15:54:09 +0100

    Update configure to check for pre-existing files
    Check that pre-existing configs exist before attempting
    to move them to prevent an error message.

--- configure.sh
+++ configure.sh
@@ -6,6 +6,9 @@ LINKS=".bbtools .xsession .xinitrc .pekwm .i3lock .wbar"
 
 for LINK in $LINKS
 do
-    mv ~/$LINK ~/${LINK}.${DATE}
+    if [ -f ~/$LINK ]
+    then
+        mv ~/$LINK ~/${LINK}.${DATE}
+    fi
     ln -s ~/git/config/$LINK ~/$LINK
 done
